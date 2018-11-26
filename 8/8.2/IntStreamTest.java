9fbe0fd8fa9f3244454ba9c44f5f09c36ffcde9a
lefeudelavie <lefeudelavie@163.com>
2018-11-26 15:54:12 +0800

    new learn file upload

---
+++ 8/8.2/IntStreamTest.java
@@ -0,0 +1,19 @@
+import java.util.stream.*;
+
+public class IntStreamTest
+{
+	public static void main(String[] args) 
+	{
+		IntStream is = IntStream.builder()
+		.add(20)
+		.add(30)
+		.add(13)
+		.add(-2)
+		.add(20)
+		.build();
+
+		System.out.println("is 所有元素的最大值:" + is.max().getAsInt());
+		//System.out.println("is 所有元素的最小值:" + is.min().getAsInt());
+		System.out.println("is 所有元素的总和:" + is.sum());
+	}
+}
