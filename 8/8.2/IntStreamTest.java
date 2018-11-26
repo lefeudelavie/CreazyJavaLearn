2f38d3f5f82b58b59e71de08fbfd0c2c32455733
lefeudelavie <lefeudelavie@163.com>
2018-11-26 23:31:00 +0800

    change intStreamTest.java

--- 8/8.2/IntStreamTest.java
+++ 8/8.2/IntStreamTest.java
@@ -16,6 +16,15 @@ public class IntStreamTest
 		//System.out.println("is 所有元素的最小值:" + is.min().getAsInt());
 		//System.out.println("is 所有元素的总和:" + is.sum());
 		//System.out.println("is 所有元素的平均值:" + is.average().getAsDouble());
-
+		//System.out.println("is 所有的元素的平方是否都大于20:"
+		//					 + is.allMatch(ele -> ele * ele >20));
+		// System.out.println("is 是否包含任何元素的平方大于20:"
+		// 					+ is.anyMatch(ele -> ele * ele > 20	));
+		//IntStream newIs = is.map(ele -> ele * 2 + 1);
+		//newIs.forEach(System.out::println);
+		//System.out.println("is 有任何元素大于20：" 
+		//					+ newIs.anyMatch(ele -> ele > 20));
+		System.out.println("过滤掉所有大于20的元素:" + is.filter(ele -> ele > 20));
+		//is.forEach(System.out::println);
 	}
 }
