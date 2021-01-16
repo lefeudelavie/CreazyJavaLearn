db4b1beb8de68666cc0bc3f90a0f7b04949f4778
lefeudelavie <kindknight2008@gmail.com>
2021-01-17 00:05:31 +0800

    change encoding for some file, and add revision file

---
+++ 4/4.6/ArrayBasicRevision.java
@@ -0,0 +1,11 @@
+public class ArrayBasicRevision {
+   public static void main(String[] args){
+       // 静态初始化
+       int[] iArr = {1,2,3,4,5};
+       // int j;
+       for (int j: iArr)
+       {
+           System.out.println(j);
+       }
+   } 
+}
